a01f6bf06c8982091070fc87195c81e0baf48047
zhaoqixiao <zhaoqixiao@tencent.com>
2021-07-12 20:03:52 +0800

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+    if args.len() < 3 { show_help(); return; }
+    let conf = parse_config(args);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(mut args: Vec<String>) -> Config {
+    let conf = Config {
+        query: args.remove(1),
+        filename: args.remove(2),
+    };
+    return conf;
+}
+
+fn show_help() {
+    let help_info = r#"
+Usage: minigrep PATTERNS FILE
+Search for PATTERNS in specific file.
+Example: minigrep hello main.c"#;
 }
